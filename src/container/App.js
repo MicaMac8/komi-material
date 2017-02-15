cf77333cce9d341d80992a6cf08f14cd18b1ce5e
Michael Maccio <m.michael.dev@gmail.com>
2017-02-15 23:46:33 +0100

    add sanitize.css

--- src/container/App.js
+++ src/container/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 
 import UserPage from './UserPage'
 
+import './app.css'
+import '../assets/sanitize.css'
+
 export default class App extends Component {
   render () {
     return <UserPage />
